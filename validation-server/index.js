b5e5dac5b572cedef4ede2f0ec68e0f06a18e7f9
shashanksola <solashashank1010@gmail.com>
2024-09-16 21:04:29 +0530

    added cors to allow origin from another ports

--- validation-server/index.js
+++ validation-server/index.js
@@ -1,8 +1,13 @@
 const express = require("express");
 const axios = require("axios");
+const cors = require("cors");
 
 const app = express();
 app.use(express.json());
+
+app.use(cors({
+  origin: 'http://localhost:3001' // Allow specific origin
+}));
 
 const detectBird = async (birdLink) => {
   try {
