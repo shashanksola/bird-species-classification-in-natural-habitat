f9c3ebd8dd0885fbf0967da741b8a5fc411ffaf2
shashanksola <solashashank1010@gmail.com>
2024-09-19 15:35:32 +0530

    added image preview to dropzone

--- frontend/src/components/Dropzone.jsx
+++ frontend/src/components/Dropzone.jsx
@@ -1,14 +1,68 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { useDropzone } from 'react-dropzone';
 
-function Dropzone(props) {
-    const { acceptedFiles, getRootProps, getInputProps } = useDropzone();
+const thumbsContainer = {
+    display: 'flex',
+    flexDirection: 'row',
+    flexWrap: 'wrap',
+    marginTop: 16
+};
 
-    const files = acceptedFiles.map(file => (
-        <li key={file.path}>
-            {file.path} - {file.size} bytes
-        </li>
+const thumb = {
+    display: 'inline-flex',
+    borderRadius: 2,
+    border: '1px solid #eaeaea',
+    marginBottom: 8,
+    marginRight: 8,
+    width: 100,
+    height: 100,
+    padding: 4,
+    boxSizing: 'border-box'
+};
+
+const thumbInner = {
+    display: 'flex',
+    minWidth: 0,
+    overflow: 'hidden'
+};
+
+const img = {
+    display: 'block',
+    width: 'auto',
+    height: '100%'
+};
+
+
+function Dropzone(props) {
+    const [files, setFiles] = useState([]);
+    const { getRootProps, getInputProps } = useDropzone({
+        accept: {
+            'image/*': []
+        },
+        onDrop: acceptedFiles => {
+            setFiles(acceptedFiles.map(file => Object.assign(file, {
+                preview: URL.createObjectURL(file)
+            })));
+        }
+    });
+
+    const thumbs = files.map(file => (
+        <div style={thumb} key={file.name}>
+            <div style={thumbInner}>
+                <img
+                    src={file.preview}
+                    style={img}
+                    // Revoke data uri after image is loaded
+                    onLoad={() => { URL.revokeObjectURL(file.preview) }}
+                />
+            </div>
+        </div>
     ));
+
+    useEffect(() => {
+        // Make sure to revoke the data uris to avoid memory leaks, will run on unmount
+        return () => files.forEach(file => URL.revokeObjectURL(file.preview));
+    }, []);
 
     return (
         <section className="container">
@@ -16,9 +70,8 @@ function Dropzone(props) {
                 <input {...getInputProps()} />
                 <p>Drag 'n' drop some files here, or click to select files</p>
             </div>
-            <aside>
-                <h4>Files</h4>
-                <ul>{files}</ul>
+            <aside style={thumbsContainer}>
+                {thumbs}
             </aside>
         </section>
     );
