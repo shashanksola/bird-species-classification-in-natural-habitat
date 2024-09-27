a783b3a86c92bac95efa0ed19069a29b1d782acc
shashanksola <solashashank1010@gmail.com>
2024-09-27 15:26:09 +0530

    modified validation-server js code, added functions for tags classification, needs further checks

--- backend/validation-server/index.js
+++ backend/validation-server/index.js
@@ -1,6 +1,8 @@
+require('dotenv').config();
 const express = require("express");
 const axios = require("axios");
 const cors = require("cors");
+
 
 const app = express();
 app.use(express.json());
@@ -15,6 +17,7 @@ const detectBird = async (birdLink) => {
       image_url: birdLink,
     });
 
+    //getEnvironmentTags(birdLink); invoke from endpoint code
     return response.data.isBird;
   } catch (error) {
     console.error(`Error: ${error}`);
@@ -22,21 +25,45 @@ const detectBird = async (birdLink) => {
   }
 };
 
+const getEnvironmentTags = async (birdLink) => { // needs updation
+  try {
+    const response = await axios.post("http://127.0.0.1:8000/envTags/", {
+      image_url: birdLink,
+    });
+
+    return response.data.tags;
+  } catch (error) {
+    console.error(`Error: ${error}`);
+    throw new Error("Error in tags classification");
+  }
+}
+
 app.post("/validate/", async (req, res) => {
   const birdLink = req.body.birdLink || "https://images.pexels.com/photos/349758/hummingbird-bird-birds-349758.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1";
 
   try {
     const isBird = await detectBird(birdLink);
-    res.send({ isBird });
+    res.status(200).send({ isBird });
+  } catch (error) {
+    res.status(500).send(`Error: ${error.message}`);
+  }
+});
+
+app.post("/tagClassifier/", async (req, res) => { // needs updation
+  const birdLink = req.body.birdLink || "https://images.pexels.com/photos/349758/hummingbird-bird-birds-349758.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1";
+
+  try {
+    const isBird = await getEnvironmentTags(birdLink);
+    res.status(200).send({ tags });
   } catch (error) {
     res.status(500).send(`Error: ${error.message}`);
   }
 });
 
 app.get("/", (req, res) => {
-  res.send("Hello World!");
+  res.send("The server is running");
 });
 
-app.listen(3000, () => {
-  console.log("Server is running on http://localhost:3000");
+app.listen(process.env.PORT || 3000, () => {
+  console.log(`Server is running on http://localhost:${process.env.PORT}`);
 });
