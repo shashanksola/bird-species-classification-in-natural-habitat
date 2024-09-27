3a20a7e29c290d3a144a10dd669b1407dbcee9b1
shashanksola <solashashank1010@gmail.com>
2024-09-27 15:59:02 +0530

    user can now select the image that need to be processed

--- frontend/src/components/Dropzone.jsx
+++ frontend/src/components/Dropzone.jsx
@@ -14,7 +14,7 @@ const thumb = {
     border: '1px solid #eaeaea',
     marginBottom: 8,
     marginRight: 8,
-    width: 100,
+    width: 'auto',
     height: 100,
     padding: 4,
     boxSizing: 'border-box'
@@ -29,32 +29,45 @@ const thumbInner = {
 const img = {
     display: 'block',
     width: 'auto',
-    height: '100%'
+    height: '100%',
+};
+
+const selectedImg = {
+    display: 'block',
+    width: '100%',
+    height: '98%',
+    border: '2px solid lightblue'
 };
 
 
 function Dropzone(props) {
     const [files, setFiles] = useState([]);
+    const [selectedFile, setSelectedFile] = useState("");
     const { getRootProps, getInputProps } = useDropzone({
         accept: {
             'image/*': []
         },
         onDrop: acceptedFiles => {
-            //props.onInputChange(acceptedFiles.preview);
             setFiles(acceptedFiles.map(file => Object.assign(file, {
                 preview: URL.createObjectURL(file)
             })));
         }
     });
 
+    const updateSelectedFile = (localURL) => {
+        console.log(localURL);
+        setSelectedFile(localURL);
+    }
+
     const thumbs = files.map(file => (
         <div style={thumb} key={file.name}>
             <div style={thumbInner}>
                 <img
                     src={file.preview}
-                    style={img}
+                    style={file.preview === selectedFile ? selectedImg : img}
                     // Revoke data uri after image is loaded
-                    onLoad={() => { URL.revokeObjectURL(file.preview) }}
+                    // onLoad={() => { URL.revokeObjectURL(file.preview) }}
+                    onClick={() => updateSelectedFile(file.preview)}
                 />
             </div>
         </div>
@@ -67,13 +80,14 @@ function Dropzone(props) {
 
     return (
         <section className="container">
-            <div {...getRootProps({ className: 'dropzone disabled' })}>
+            <div {...getRootProps({ className: 'dropzone' })}>
                 <input {...getInputProps()} />
                 <p>Drag 'n' drop some files here, or click to select files</p>
             </div>
             <aside style={thumbsContainer}>
                 {thumbs}
             </aside>
+            {files.length >= 1 ? <p style={{ color: 'white' }}>*Click on the image to select a image*</p> : null}
         </section>
     );
 }
